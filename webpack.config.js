22ee33a1eb89351a80b48b51f75f65de2bca84ef
Miron Fissenko <mirfees@gmail.com>
2026-06-15 21:02:34 +0500

    RFC: конфиг webpack и структура кода

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
+
     entry: './src/index.js',
 
+
+
     output: {
-        filename: 'main.js',
+        filename: 'analytics.min.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+
+        library: {
+            name: 'Analytics',   // Имя переменной, которая появится в window (window.Analytics)
+            type: 'window',      // Куда именно экспортировать
+            export: 'default',   // Что экспортировать (экспорт по умолчанию)
+        },
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_classnames: true, // Сохраняет имена классов
+                    keep_fnames: true,     // Сохраняет имена функций и методов
+                    mangle: true,          // Все остальное (локальные переменные) сжимаем
+                },
+            }),
+        ],
     },
 }
